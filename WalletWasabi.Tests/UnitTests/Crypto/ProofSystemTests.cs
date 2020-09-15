cfc56d00d94881f148071e0c4e9f4c49e91fd550
lontivero <lucasontivero@gmail.com>
2020-09-15 15:39:01 -0300

    Proof od Serial

--- WalletWasabi.Tests/UnitTests/Crypto/ProofSystemTests.cs
+++ WalletWasabi.Tests/UnitTests/Crypto/ProofSystemTests.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 using NBitcoin.Secp256k1;
 using WalletWasabi.Crypto;
 using WalletWasabi.Crypto.Groups;
 using WalletWasabi.Crypto.Randomness;
 using WalletWasabi.Crypto.ZeroKnowledge;
+using WalletWasabi.Crypto.ZeroKnowledge.NonInteractive;
 using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.Crypto
@@ -33,24 +35,38 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 			var t = rnd.GetScalar();
 			var mac = MAC.ComputeMAC(coordinatorKey, Ma, t);
 
-			var coordinatorStatement = ProofSystem.CreateStatement(coordinatorParameters, mac.V, Ma, t);
-			var proverBuilder = ProofSystem.CreateProver(coordinatorStatement, coordinatorKey);
+			var proverBuilder = ProofSystem.ProveIssuerParameters(coordinatorKey, mac, Ma);
 			var macProver = proverBuilder(rnd);
 			var proofOfMac = macProver();
 
 			// The client receives the MAC and the proofOfMac which let the client know that the MAC 
 			// was generated with the coordinator's secret key.
-			var clientStatement = ProofSystem.CreateStatement(coordinatorParameters, mac.V, Ma, mac.T);
-			var verifierBuilder = ProofSystem.CreateVerifier(clientStatement);
+			var verifierBuilder = ProofSystem.VerifyIssuerParameters(coordinatorParameters, mac, Ma);
 			var macVerifier = verifierBuilder(proofOfMac);
 			var isValidProof = macVerifier();
 
 			Assert.True(isValidProof);
+			////////
+
+			var corruptedResponses = proofOfMac.Responses.ToArray();
+			corruptedResponses[0] = new ScalarVector(corruptedResponses[0].Reverse());
+			var invalidProofOfMac = new Proof(proofOfMac.PublicNonces, corruptedResponses);
+			macVerifier = verifierBuilder(invalidProofOfMac);
+			isValidProof = macVerifier();
+
+			Assert.False(isValidProof);
+
+			var corruptedPublicNonces = new GroupElementVector(proofOfMac.PublicNonces.Reverse());
+			invalidProofOfMac = new Proof(corruptedPublicNonces, proofOfMac.Responses);
+			macVerifier = verifierBuilder(invalidProofOfMac);
+			isValidProof = macVerifier();
+
+			Assert.False(isValidProof);
 		}
 
 		[Fact]
 		[Trait("UnitTest", "UnitTest")]
-		public void CanProveAndVerifyNULL()
+		public void CanProveAndVerifyZeroAmount()
 		{
 			// The client wants to request a zero amount credential and it needs to prove
 			// that the bliended amount is indeed zero.  
@@ -59,17 +75,39 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 			var blindingFactor = rnd.GetScalar();
 			var Ma = amount * Generators.Gg + blindingFactor * Generators.Gh;
 
-			var clientStatement = ProofSystem.CreateStatement(Ma, Generators.Gh);
-			var proverBuilder = ProofSystem.CreateProver(clientStatement, blindingFactor);
+			var proverBuilder = ProofSystem.ProveZeroAmount(Ma, blindingFactor);
 			var nullProver = proverBuilder(rnd);
 			var proofOfNull = nullProver();
 
-
 			// The coordinator must verify the blinded amout is zero
-			var coordinatorStatement = ProofSystem.CreateStatement(Ma, Generators.Gh);
-			var verifierBuilder = ProofSystem.CreateVerifier(coordinatorStatement);
+			var verifierBuilder = ProofSystem.VerifyZeroAmount(Ma);
 			var nullVerifier = verifierBuilder(proofOfNull);
 			var isValidProof = nullVerifier();
+
+			Assert.True(isValidProof);
+		}
+
+		[Fact]
+		[Trait("UnitTest", "UnitTest")]
+		public void CanProveAndVerifySerialNumbers()
+		{
+			var rnd = new SecureRandom();
+			var z = rnd.GetScalar();
+			var r = rnd.GetScalar();
+			var a = rnd.GetScalar();
+
+			var witness = new ScalarVector(z, a, r);
+			var Ca = z * Generators.Ga + r * Generators.Gh + a * Generators.Gg;
+			var S = r * Generators.Gs;
+
+			var proverBuilder = ProofSystem.ProveSerialNumber(witness, Ca, S);
+			var serialNumberProver = proverBuilder(rnd);
+			var proofOfSerialNumber = serialNumberProver();
+
+			// The coordinator must verify the blinded amout is zero
+			var verifierBuilder = ProofSystem.VerifySerialNumber(Ca, S);
+			var serialNumberVerifier = verifierBuilder(proofOfSerialNumber);
+			var isValidProof = serialNumberVerifier();
 
 			Assert.True(isValidProof);
 		}
--- WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
@@ -9,36 +9,85 @@ namespace WalletWasabi.Crypto.ZeroKnowledge
 	public static class ProofSystem
 	{
 		private static GroupElement Inf = GroupElement.Infinity;
-		private static Transcript Transcript => new Transcript(Encoding.UTF8.GetBytes("proof-of-parameters"));
+
+		#region Issuer parameters
+		public static LinearRelation.Statement CreateIssuerParametersStatement(CoordinatorParameters coordinatorParameters, MAC mac, GroupElement Ma) =>
+			new LinearRelation.Statement(new[,]
+			{
+				{ coordinatorParameters.Cw, Generators.Gw, Generators.Gwp, Inf, Inf, Inf },
+				{ Generators.GV - coordinatorParameters.I, Inf, Inf, Generators.Gx0, Generators.Gx1, Generators.Ga },
+				{ mac.V, Generators.Gw, Inf, MAC.GenerateU(mac.T), mac.T * MAC.GenerateU(mac.T), Ma }
+			});
+
+		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces ProveIssuerParameters(CoordinatorSecretKey coordinatorSecretKey, MAC mac, GroupElement Ma) =>
+			CreateProver(
+				TranscriptProofIssuerParameters,
+				CreateIssuerParametersStatement(coordinatorSecretKey.ComputeCoordinatorParameters(), mac, Ma), 
+				coordinatorSecretKey.ToScalarVector());
+
+		public static NonInteractive.FiatShamirTransform.VerifierCommitToNonces VerifyIssuerParameters(CoordinatorParameters coordinatorParameters, MAC mac, GroupElement Ma) =>
+			CreateVerifier(
+				TranscriptProofIssuerParameters,
+				CreateIssuerParametersStatement(coordinatorParameters, mac, Ma));
+
+		private static Transcript TranscriptProofIssuerParameters => new Transcript(Encoding.UTF8.GetBytes("proof-of-issuer-parameters"));
+
+		#endregion Issuer parameters
+
+		#region Zero amount
+
+		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces ProveZeroAmount(GroupElement Ma, Scalar blindingFactor) =>
+			CreateProver(
+				TranscriptProofZeroAmount,
+				CreateStatement(Ma, Generators.Gh), 
+				blindingFactor);
+
+		public static NonInteractive.FiatShamirTransform.VerifierCommitToNonces VerifyZeroAmount(GroupElement Ma) =>
+			CreateVerifier(
+				TranscriptProofZeroAmount,
+				CreateStatement(Ma, Generators.Gh));
+
+		private static Transcript TranscriptProofZeroAmount => new Transcript(Encoding.UTF8.GetBytes("proof-of-zero-amount"));
+
+		#endregion Zero amount
+
+		#region Serial number
+
+		public static LinearRelation.Statement CreateSerialNumberStatement(GroupElement Ca, GroupElement S) =>
+			new LinearRelation.Statement(new[,]
+			{
+				// z a r
+				{ Ca, Generators.Ga, Generators.Gg,  Generators.Gh },
+				{ S, Inf, Inf, Generators.Gs }
+			});
+
+		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces ProveSerialNumber(ScalarVector witness, GroupElement Ca, GroupElement S) =>
+			CreateProver(
+				TranscriptProofSerialNumber,
+				CreateSerialNumberStatement(Ca, S), 
+				witness);
+
+		public static NonInteractive.FiatShamirTransform.VerifierCommitToNonces VerifySerialNumber(GroupElement Ca, GroupElement S) =>
+			CreateVerifier(
+				TranscriptProofSerialNumber,
+				CreateSerialNumberStatement(Ca, S));
+
+		private static Transcript TranscriptProofSerialNumber => new Transcript(Encoding.UTF8.GetBytes("proof-of-serial-number"));
+
+		#endregion Serial number
 
 		public static LinearRelation.Statement CreateStatement(GroupElement publicPoint, params GroupElement[] generator) =>
 			new LinearRelation.Statement(new Equation(publicPoint, new GroupElementVector(generator)));
 
-		public static LinearRelation.Statement CreateStatement(CoordinatorParameters coordinatorParameters, GroupElement V, GroupElement Ma, Scalar t) =>
-			new LinearRelation.Statement(
-				new GroupElementVector(coordinatorParameters.Cw, Generators.GV - coordinatorParameters.I, V)
-					.Zip(ProofOfParametersGenerators(MAC.GenerateU(t), Ma, t), (publicPoint, groupElement) => new Equation (publicPoint, groupElement)));
 
-		public static NonInteractive.FiatShamirTransform.VerifierCommitToNonces CreateVerifier(LinearRelation.Statement statement) =>
-			new NonInteractive.FiatShamirTransform.Verifier(statement).CommitToStatements(Transcript);
+		private static NonInteractive.FiatShamirTransform.VerifierCommitToNonces CreateVerifier(Transcript transcript, LinearRelation.Statement statement) =>
+			new NonInteractive.FiatShamirTransform.Verifier(statement).CommitToStatements(transcript);
 
-		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(LinearRelation.Statement statement, CoordinatorSecretKey coordinatorSecretKey) =>
-			CreateProver(statement, coordinatorSecretKey.ToScalarVector());
+		private static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(Transcript transcript, LinearRelation.Statement statement, params Scalar[] witness) =>
+			CreateProver(transcript, statement, new ScalarVector(witness));
 
-		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(LinearRelation.Statement statement, params Scalar[] witness) =>
-			CreateProver(statement, new ScalarVector(witness));
-
-		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(LinearRelation.Statement statement, ScalarVector witness) =>
-			new NonInteractive.FiatShamirTransform.Prover(statement.ToKnowledge(witness)).CommitToStatements(Transcript);
-
-		private static GroupElementVector[] ProofOfParametersGenerators(GroupElement U, GroupElement Ma, Scalar t) =>
-			new GroupElementVector[]
-			{
-				// coordinator's iparams W, Wp, X0, X1, Ya
-				new GroupElementVector( Generators.Gw, Generators.Gwp, Inf, Inf, Inf ),
-				new GroupElementVector( Inf, Inf, Generators.Gx0, Generators.Gx1, Generators.Ga ),
-				new GroupElementVector( Generators.Gw, Inf, U, t * U, Ma )
-			};
+		private static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(Transcript transcript, LinearRelation.Statement statement, ScalarVector witness) =>
+			new NonInteractive.FiatShamirTransform.Prover(statement.ToKnowledge(witness)).CommitToStatements(transcript);
 
 		private static ScalarVector ToScalarVector(this CoordinatorSecretKey coordinatorSecretKey) =>
 			new ScalarVector(

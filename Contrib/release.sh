19085c22fb7dee2e8f4136bebcf92f9f443731b3
Lucas Ontivero <lucasontivero@gmail.com>
2024-07-16 12:38:25 -0300

    test

--- Contrib/release.sh
+++ Contrib/release.sh
@@ -42,11 +42,23 @@ PACKAGES_DIR="packages"
 # Common name for all packages
 PACKAGE_FILE_NAME_PREFIX="Wasabi-$VERSION"
 
-# Supported platforms
-PLATFORMS=("win-x64" "linux-x64" "osx-x64" "osx-arm64")
+if [ "$1" = "wininstaller" ]; then
+  # Supported platforms
+  PLATFORMS=("win-x64")
+  CREATE_WINDOWS_INSTALLER="yes"
+  CREATE_DEBIAN_PACKAGE="no"
+else
+  # Supported platforms
+  PLATFORMS=("linux-x64" "osx-x64" "osx-arm64")
+  CREATE_WINDOWS_INSTALLER="no"
+  CREATE_DEBIAN_PACKAGE="yes"
+fi
 
-# Remove the build directory if it exists
-rm -rf $BUILD_DIR
+# Remove the build directory if it exists and recreate it
+rm -rf "$BUILD_DIR" && mkdir -p "$BUILD_DIR"
+
+# Create packages directory (where all final packages are saved)
+rm -rf "$BUILD_DIR" && mkdir -p "$PACKAGES_DIR"
 
 #------------------------------------------------------------------------------------#
 # BUILD DESKTOP FOR ALL PLATFORMS                                                    #
@@ -102,9 +114,11 @@ for PLATFORM in "${PLATFORMS[@]}"; do
   if [[ "${PLATFORM_PREFIX}" == "osx" ]]; then
     ALTER_PLATFORM="macOS${PLATFORM:3}"
   fi
+done
 
+# Loop through each platform and create zip/tar.gz
+for PLATFORM in "${PLATFORMS[@]}"; do
   # Create compressed package files (.zip and .tar.gz)
-  mkdir -p "$PACKAGES_DIR"
   PACKAGE_FILE_NAME=$PACKAGE_FILE_NAME_PREFIX-$ALTER_PLATFORM
   if [[ "${PLATFORM_PREFIX}" == "lin" ]]; then
     tar -pczvf $PACKAGES_DIR/$PACKAGE_FILE_NAME.tar.gz $OUTPUT_DIR
@@ -113,93 +127,127 @@ for PLATFORM in "${PLATFORMS[@]}"; do
   fi
 done
 
+
 #------------------------------------------------------------------------------------#
 # CREATE DEBIAN PACKAGE                                                              #
 #------------------------------------------------------------------------------------#
-# Create .deb package
-DEBIAN_PACKAGE_DIR=$BUILD_DIR/deb
-DEBIAN=$DEBIAN_PACKAGE_DIR/DEBIAN
-DEBIAN_USR=$DEBIAN_PACKAGE_DIR/usr
-DEBIAN_BIN=$DEBIAN_USR/local/bin
+if [ "$CREATE_DEBIAN_PACKAGE" = "yes" ]; then
+  # Create .deb package
+  DEBIAN_PACKAGE_DIR=$BUILD_DIR/deb
+  DEBIAN=$DEBIAN_PACKAGE_DIR/DEBIAN
+  DEBIAN_USR=$DEBIAN_PACKAGE_DIR/usr
+  DEBIAN_BIN=$DEBIAN_USR/local/bin
 
-# Create necessary directories
-mkdir -p $DEBIAN
-mkdir -p $DEBIAN_BIN
-mkdir -p $DEBIAN_USR/share/{applications,icons/hicolor}
+  # Create necessary directories
+  mkdir -p $DEBIAN
+  mkdir -p $DEBIAN_BIN
+  mkdir -p $DEBIAN_USR/share/{applications,icons/hicolor}
 
-# Copy icon files
-for ICON_FILE in "$DESKTOP"/Assets/WasabiLogo*.png; do
-  SIZE=$(echo "$ICON_FILE" | grep -oP '\d+')
-  ICON_DIR="$DEBIAN_USR/share/icons/hicolor/${SIZE}x${SIZE}/app"
-  mkdir -p "$ICON_DIR"
-  cp "$ICON_FILE" "$ICON_DIR/$EXECUTABLE_NAME.png"
-done
+  # Copy icon files
+  for ICON_FILE in "$DESKTOP"/Assets/WasabiLogo*.png; do
+    SIZE=$(echo "$ICON_FILE" | grep -oP '\d+')
+    ICON_DIR="$DEBIAN_USR/share/icons/hicolor/${SIZE}x${SIZE}/app"
+    mkdir -p "$ICON_DIR"
+    cp "$ICON_FILE" "$ICON_DIR/$EXECUTABLE_NAME.png"
+  done
 
-# Calculate package size (in kilobytes)
-DEBIAN_PACKAGE_SIZE=$(du -s $BUILD_DIR/linux-x64 | cut -f1)
+  # Calculate package size (in kilobytes)
+  DEBIAN_PACKAGE_SIZE=$(du -s $BUILD_DIR/linux-x64 | cut -f1)
 
-# Create the control file content
-DEBIAN_CONTROL_FILE_CONTENT="Package: ${EXECUTABLE_NAME}
-Priority: optional
-Section: utils
-Maintainer: Wasabi Wallet Team
-Version: ${VERSION}
-Homepage: https://wasabiwallet.io
-Vcs-Git: git://github.com/WalletWasabi/WalletWasabi.git
-Vcs-Browser: https://github.com/WalletWasabi/WalletWasabi
-Architecture: amd64
-License: Open Source (MIT)
-Installed-Size: ${DEBIAN_PACKAGE_SIZE}
-Recommends: policykit-1
-Description: open-source, non-custodial, privacy focused Bitcoin wallet
-  Built-in Tor, coinjoin, payjoin and coin control features."
+  # Create the control file content
+  DEBIAN_CONTROL_FILE_CONTENT="Package: ${EXECUTABLE_NAME}
+  Priority: optional
+  Section: utils
+  Maintainer: Wasabi Wallet Team
+  Version: ${VERSION}
+  Homepage: https://wasabiwallet.io
+  Vcs-Git: git://github.com/WalletWasabi/WalletWasabi.git
+  Vcs-Browser: https://github.com/WalletWasabi/WalletWasabi
+  Architecture: amd64
+  License: Open Source (MIT)
+  Installed-Size: ${DEBIAN_PACKAGE_SIZE}
+  Recommends: policykit-1
+  Description: open-source, non-custodial, privacy focused Bitcoin wallet
+    Built-in Tor, coinjoin, payjoin and coin control features."
 
-echo "${DEBIAN_CONTROL_FILE_CONTENT}" > $DEBIAN/control
+  echo "${DEBIAN_CONTROL_FILE_CONTENT}" > $DEBIAN/control
 
-# Post-installation script content
-USR_LOCAL_BIN_DIR="/usr/local/bin"
-INSTALL_DIR="${USR_LOCAL_BIN_DIR}/wasabiwallet"
-DEBIAN_POST_INST_SCRIPT_CONTENT="#!/usr/bin/env sh
-${INSTALL_DIR}/Microservices/Binaries/lin64/hwi installudevrules
-exit 0"
-echo "${DEBIAN_POST_INST_SCRIPT_CONTENT}" > $DEBIAN/postinst
-chmod 0775 ${DEBIAN}/postinst
+  # Post-installation script content
+  USR_LOCAL_BIN_DIR="/usr/local/bin"
+  INSTALL_DIR="${USR_LOCAL_BIN_DIR}/wasabiwallet"
+  DEBIAN_POST_INST_SCRIPT_CONTENT="#!/usr/bin/env sh
+  ${INSTALL_DIR}/Microservices/Binaries/lin64/hwi installudevrules
+  exit 0"
+  echo "${DEBIAN_POST_INST_SCRIPT_CONTENT}" > $DEBIAN/postinst
+  chmod 0775 ${DEBIAN}/postinst
 
-# Create the desktop file content
-DEBIAN_DESKTOP_CONTENT="[Desktop Entry]
-Type=Application
-Name=Wasabi Wallet
-StartupWMClass=Wasabi Wallet
-GenericName=Bitcoin Wallet
-Comment=Privacy focused Bitcoin wallet.
-Icon=${EXECUTABLE_NAME}
-Terminal=false
-Exec=${EXECUTABLE_NAME}
-Categories=Office;Finance;
-Keywords=bitcoin;wallet;crypto;blockchain;wasabi;privacy;anon;awesome;"
+  # Create the desktop file content
+  DEBIAN_DESKTOP_CONTENT="[Desktop Entry]
+  Type=Application
+  Name=Wasabi Wallet
+  StartupWMClass=Wasabi Wallet
+  GenericName=Bitcoin Wallet
+  Comment=Privacy focused Bitcoin wallet.
+  Icon=${EXECUTABLE_NAME}
+  Terminal=false
+  Exec=${EXECUTABLE_NAME}
+  Categories=Office;Finance;
+  Keywords=bitcoin;wallet;crypto;blockchain;wasabi;privacy;anon;awesome;"
 
-# Write the content to the file
-DEBIAN_DESKTOP="${DEBIAN_USR}/share/applications/${EXECUTABLE_NAME}.desktop"
-echo "${DEBIAN_DESKTOP_CONTENT}" > $DEBIAN_DESKTOP
-sudo chmod 0644 $DEBIAN_DESKTOP
+  # Write the content to the file
+  DEBIAN_DESKTOP="${DEBIAN_USR}/share/applications/${EXECUTABLE_NAME}.desktop"
+  echo "${DEBIAN_DESKTOP_CONTENT}" > $DEBIAN_DESKTOP
+  sudo chmod 0644 $DEBIAN_DESKTOP
 
-# Copy the build to into the debian package structure
-cp -r $BUILD_DIR/linux-x64 $DEBIAN_BIN/wasabiwallet
+  # Copy the build to into the debian package structure
+  cp -r $BUILD_DIR/linux-x64 $DEBIAN_BIN/wasabiwallet
 
-# Create wrapper scripts
-echo "#!/usr/bin/env sh
-${INSTALL_DIR}/${EXECUTABLE_NAME} \$@" > ${DEBIAN_BIN}/${EXECUTABLE_NAME}
+  # Create wrapper scripts
+  echo "#!/usr/bin/env sh
+  ${INSTALL_DIR}/${EXECUTABLE_NAME} \$@" > ${DEBIAN_BIN}/${EXECUTABLE_NAME}
 
-echo "#!/usr/bin/env sh
-${INSTALL_DIR}/${EXECUTABLE_NAME}d \$@" > ${DEBIAN_BIN}/${EXECUTABLE_NAME}d
+  echo "#!/usr/bin/env sh
+  ${INSTALL_DIR}/${EXECUTABLE_NAME}d \$@" > ${DEBIAN_BIN}/${EXECUTABLE_NAME}d
 
-# Remove execution to everything except for executables and their wrapper scripts
-sudo chmod -R 0644 ${DEBIAN_BIN}/wasabiwallet
-sudo chmod 0775 ${DEBIAN_BIN}/wasabiwallet/${EXECUTABLE_NAME}{,d}
-sudo chmod 0775 ${DEBIAN_BIN}/${EXECUTABLE_NAME}{,d}
+  # Remove execution to everything except for executables and their wrapper scripts
+  sudo chmod -R 0644 ${DEBIAN_BIN}/wasabiwallet
+  sudo chmod 0775 ${DEBIAN_BIN}/wasabiwallet/${EXECUTABLE_NAME}{,d}
+  sudo chmod 0775 ${DEBIAN_BIN}/${EXECUTABLE_NAME}{,d}
 
-# Build the .deb package
-dpkg --build "${DEBIAN_PACKAGE_DIR}" "$PACKAGES_DIR/${PACKAGE_FILE_NAME_PREFIX}.deb"
+  # Build the .deb package
+  dpkg --build "${DEBIAN_PACKAGE_DIR}" "$PACKAGES_DIR/${PACKAGE_FILE_NAME_PREFIX}.deb"
+fi
+
+if [ "$CREATE_WINDOWS_INSTALLER" = "yes" ]; then
+  #------------------------------------------------------------------------------------#
+  # CREATE WINDOWS INSTALLER                                                           #
+  #------------------------------------------------------------------------------------#
+  WIX_BIN_DIR="$WIX/bin"
+  WINDOWS_INSTALLER_DIR="WalletWasabi.WindowsInstaller"
+  CANDLE_EXE="$WIX_BIN_DIR/"candle
+  LIGHT_EXE="$WIX_BIN_DIR"/light
+  BUILD_INSTALLER_DIR="$BUILD_DIR/win-installer"
+
+  mkdir -p BUILD_INSTALLER_DIR
+
+  # Compile the .wxs file to .wixobj
+  $CANDLE_EXE \
+      $WINDOWS_INSTALLER_DIR/*.wxs \
+      -dBasePath=$BUILD_DIR \
+      -dBuildVersion=$VERSION \
+      -dDesktopProjectDir=$DESKTOP \
+      -dConfiguration=Release \
+      -dPlatform=win-x64 \
+      -out "$BUILD_INSTALLER_DIR"
+
+  # Link the .wixobj file to create the .msi installer
+  $LIGHT_EXE \
+      $WINDOWS_INSTALLER_DIR/*.wixobj \
+      -loc $WINDOWS_INSTALLER_DIR/Common.wxl \
+      -ext "$WIX_BIN_DIR/WixUIExtension.dll" \
+      -ext "$WIX_BIN_DIR/WixUtilExtension.dll" \
+      -out $PACKAGES_DIR/$PACKAGE_FILE_NAME_PREFIX.msi
+fi
 
 # Unstash changes if there were any
 if git stash list | head -1 | grep -q "$STASH_MESSAGE"; then
